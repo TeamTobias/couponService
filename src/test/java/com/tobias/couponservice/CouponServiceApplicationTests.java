f25b7ea0ed8897e5b6b5f8dc7c0ab168e76271b1
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-22 14:55:51 +0900

    init commit

---
+++ src/test/java/com/tobias/couponservice/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
