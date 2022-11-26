a1c2d6f2f417086d670e20bd76c2d6c1bbcba381
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-27 03:16:03 +0900

    TEAM-607 판매자 쿠폰 서비스 퍼사드

--- src/main/java/com/tobias/couponservice/inner/service/BrandCouponRequestService.java
+++ src/main/java/com/tobias/couponservice/inner/service/BrandCouponRequestService.java
@@ -1,7 +1,19 @@
 package com.tobias.couponservice.inner.service;
 
+import com.tobias.couponservice.inner.domain.entity.Coupon;
+
+import java.util.List;
+
+/*
+* @Description : 판매자 쿠폰 등록 요청 처리
+* @Author : ImKunYoung
+* */
 public interface BrandCouponRequestService {
+
     /*판매자 쿠폰 등록 요청 조회 */
+    List<Coupon> findBrandCouponRequest();
 
     /*판매자 쿠폰 등록 요청 처리*/
+    void processBrandCouponRequest(String permitStatus, Long couponId);
+
 }
