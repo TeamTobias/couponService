245c7452b867ca1686ca9dfe4e63f13933317345
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-27 03:26:06 +0900

    TEAM-607 promotionCouponSErvice 구현체, 테스트 클래스 생성

---
+++ src/main/java/com/tobias/couponservice/inner/serviceImpl/PromotionCouponServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tobias.couponservice.inner.serviceImpl;
+
+import com.tobias.couponservice.inner.domain.entity.Coupon;
+import com.tobias.couponservice.inner.service.PromotionCouponService;
+
+import java.util.List;
+
+public class PromotionCouponServiceImpl implements PromotionCouponService {
+
+    @Override
+    public void registerPromotionCoupon(String managerId) {
+
+    }
+
+    @Override
+    public List<Coupon> findPromotionCoupon() {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/tobias/couponservice/inner/service/PromotionCouponServiceTests.java
@@ -0,0 +1,12 @@
+package com.tobias.couponservice.inner.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootTest
+@Profile("dev")
+public class PromotionCouponServiceTests {
+
+
+
+}
