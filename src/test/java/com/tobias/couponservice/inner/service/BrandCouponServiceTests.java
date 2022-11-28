48b1b0371aa99b147a81bd48c492193a002e49e8
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-28 12:11:54 +0900

    TEAM-150 BrandCouponServiceTests 빈 함수 작성

--- src/test/java/com/tobias/couponservice/inner/service/BrandCouponServiceTests.java
+++ src/test/java/com/tobias/couponservice/inner/service/BrandCouponServiceTests.java
@@ -1,4 +1,29 @@
 package com.tobias.couponservice.inner.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+
+@SpringBootTest
 public class BrandCouponServiceTests {
+
+
+    /*판매자 쿠폰 등록 요청*/
+    @Test
+    @Description("판매자 쿠폰 등록 요청")
+    public void registerBrandCouponRequest() {
+    }
+
+
+
+
+    /*판매자 쿠폰 조회*/
+    @Test
+    @Description("판매자 쿠폰 조회")
+    public void findBrandCoupon() {
+    }
+
+
+    /*판매자 쿠폰 만료*/
+
 }
