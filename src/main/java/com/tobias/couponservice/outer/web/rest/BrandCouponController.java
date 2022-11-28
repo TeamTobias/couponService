c9afed4107468da7d8eb2cc8b2b86453062784b4
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-28 20:57:44 +0900

    TEAM-595 판매자 쿠폰 등록 요청 uri 추가

--- src/main/java/com/tobias/couponservice/outer/web/rest/BrandCouponController.java
+++ src/main/java/com/tobias/couponservice/outer/web/rest/BrandCouponController.java
@@ -1,12 +1,14 @@
 package com.tobias.couponservice.outer.web.rest;
 
 import com.tobias.couponservice.outer.vo.ResponseMessage;
+import com.tobias.couponservice.outer.vo.coupon.PostBrandCouponRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,9 @@ public class BrandCouponController {
             @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class))),
     })
     @PostMapping("/v1")
-    public void createBrandCoupon() {
+    public ResponseEntity createBrandCoupon() {
+
+        return ResponseEntity.ok(new ResponseMessage("쿠폰 등록 요청이 정상 처리되었습니다."));
     }
 
 
