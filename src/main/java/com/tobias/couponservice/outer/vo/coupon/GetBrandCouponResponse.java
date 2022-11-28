dc5b9c83da629da4e8e099c7f1e6c6e02f894c6c
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-28 21:01:44 +0900

    TEAM-595 GetBrandCouponResponse vo 추가

---
+++ src/main/java/com/tobias/couponservice/outer/vo/coupon/GetBrandCouponResponse.java
@@ -0,0 +1,32 @@
+package com.tobias.couponservice.outer.vo.coupon;
+/*
+* @Description: 판매자 쿠폰 정보 조회
+*/
+
+
+import java.util.Date;
+
+public class GetBrandCouponResponse {
+
+    //- 쿠폰 인가 상태
+    private String permitStatus;
+
+    //- 쿠폰 내용
+    private String content;
+
+    //- 쿠폰 타입
+    private String type;
+
+    //- 할인 금액
+    private int discountAmount;
+
+    //- 쿠폰 적용 가능 금액
+    private int leastAmount;
+
+    //- 쿠폰 시작일
+    private Date openDate;
+
+    //- 쿠폰 종료일
+    private Date endDate;
+
+}
