d1e5f2b7e60a7951ac52f74f78d30d2b55b540d8
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-28 13:14:20 +0900

    TEAM-150 리팩터링

--- src/main/java/com/tobias/couponservice/inner/repository/CouponRepository.java
+++ src/main/java/com/tobias/couponservice/inner/repository/CouponRepository.java
@@ -3,7 +3,6 @@ package com.tobias.couponservice.inner.repository;
 import com.tobias.couponservice.inner.domain.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.image.PixelGrabber;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
--- src/main/java/com/tobias/couponservice/outer/vo/ResponseMessage.java
+++ src/main/java/com/tobias/couponservice/outer/vo/ResponseMessage.java
@@ -1,6 +1,5 @@
 package com.tobias.couponservice.outer.vo;
 
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
