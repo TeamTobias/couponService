2b573e91564e681ab1e6c70c9ad980203e6b9c94
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-28 20:54:42 +0900

    TEAM-595 PostBrandCouponRequest 생성

---
+++ src/main/java/com/tobias/couponservice/outer/vo/coupon/PostBrandCouponRequest.java
@@ -0,0 +1,31 @@
+package com.tobias.couponservice.outer.vo.coupon;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PostBrandCouponRequest {
+
+    //- 브랜드 id
+    private String brandid;
+
+    //- 쿠폰 내용
+    private String content;
+
+    //- 쿠폰 타입
+    private String type;
+
+    //- 할인 금액
+    private int discountAmount;
+
+    //- 쿠폰 적용 가능 금액
+    private int leastAmount;
+
+    //- 쿠폰 시작일
+    private Date openDate;
+
+    //- 쿠폰 종료일
+    private Date endDate;
+
+}
